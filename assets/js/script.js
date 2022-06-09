cfc86d321b9f1edef773e3bfb8c7cc161ae75dc9
Nathan Ribeiro <nathan.ribeiros7@gmail.com>
2022-06-09 16:25:48 -0300

    first commit

---
+++ assets/js/script.js
@@ -0,0 +1,17 @@
+let currentCountWrapper = document.getElementById('currentCount');
+let currentCount = 0;
+
+function increment() {
+    currentCount = currentCount + 1;
+    currentCountWrapper.innerHTML = currentCount;
+}
+
+
+function decrement() {
+    currentCount = currentCount - 1;
+    currentCountWrapper.innerHTML = currentCount;
+}
+
+if (currentCount = 0) {
+    currentCount==0;
+}
